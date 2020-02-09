a29361ff12ec7dfb9190ec69cbb35e7a3819df3d
lidianxiang <ldx2703@gmail.com>
2020-02-09 21:49:19 +0800

    第一个只出现一次的字符

---
+++ 第一个只出现一次的字符.py
@@ -0,0 +1,24 @@
+"""
+在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+"""
+
+
+class Solution:
+    """哈希表"""
+    def FirstNotRepeatingChar(self, s):
+
+        if not s:
+            return -1
+
+        store = {}
+        lis = list(s)
+
+        for i in lis:
+            if i not in store.keys():
+                store[i] = 0
+            store[i] += 1
+
+        for i in lis:
+            if store[i] == 1:
+                return s.index(i)
+        return -1
