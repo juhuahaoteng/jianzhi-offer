d936dc770344df41ac74edf2592d5370422b3bca
lidianxiang <ldx2703@gmail.com>
2020-02-15 16:19:22 +0800

    矩阵中的路径

--- 矩阵中的路径.py
+++ 矩阵中的路径.py
@@ -35,17 +35,15 @@ class Solution:
         if len(path) == pathLength:
             return True
         hasPath = False
-        if row >= 0 and row < rows and col >= 0 and col < cols and matrix[row * cols + col] == path[pathLength] and not \
-        visited[row * cols + col]:
+        if 0 <= row < rows and 0 <= col < cols and matrix[row * cols + col] == path[pathLength] and not \
+                visited[row * cols + col]:
             pathLength += 1
             visited[row * cols + col] = True
 
-            hasPath = self.hasPathCore(matrix, rows, cols, row, col - 1, path, pathLength, visited) or self.hasPathCore(
-                matrix, rows, cols, row - 1, col, path, pathLength, visited) or self.hasPathCore(matrix, rows, cols,
-                                                                                                 row, col + 1, path,
-                                                                                                 pathLength,
-                                                                                                 visited) or self.hasPathCore(
-                matrix, rows, cols, row + 1, col, path, pathLength, visited)
+            hasPath = self.hasPathCore(matrix, rows, cols, row, col - 1, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols, row - 1, col, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols,row, col + 1, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols, row + 1, col, path, pathLength, visited)
             if not hasPath:
                 pathLength -= 1
                 visited[row * cols + col] = False
