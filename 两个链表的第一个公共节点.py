de9f23322e5e99d15ca2b7c816922959a6de4f7b
lidianxiang <ldx2703@gmail.com>
2020-02-27 18:13:22 +0800

    两个链表的第一个公共节点

--- 两个链表的第一个公共节点.py
+++ 两个链表的第一个公共节点.py
@@ -19,6 +19,7 @@ class Solution:
     def FindFirstCommonNode(self, pHead1, pHead2):
         # 特判
         if not pHead1 or not pHead2:
+
             return None
 
         p1, p2 = pHead1, pHead2
@@ -48,3 +49,17 @@ class Solution:
             pHead2 = pHead2.next
 
         return None
+
+
+class Solution2:
+    def FindFirstCommonNode(self, pHead1, pHead2):
+        # write code here
+        if not pHead1 or not pHead2:
+            return None
+
+        p1, p2 = pHead1, pHead2
+
+        while p1 != p2:
+            p1 = pHead2 if not p1 else p1.next
+            p2 = pHead1 if not p2 else p2.next
+        return p1
