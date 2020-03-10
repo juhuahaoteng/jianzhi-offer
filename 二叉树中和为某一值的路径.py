b8f102457c6f8b2f37205c52d74a5da82d7575b1
lidianxiang <ldx2703@gmail.com>
2020-03-10 23:24:58 +0800

    二叉树中和为某一值的路径

--- 二叉树中和为某一值的路径.py
+++ 二叉树中和为某一值的路径.py
@@ -15,20 +15,22 @@ class TreeNode:
 class Solution:
     """深度优先搜索"""
     def FindPath(self, root, expectNumber):
-
+        # 当没有根节点或者根节点的值大于expectNumber的值，直接返回空列表
         if not root or root.val > expectNumber:
             return []
-
+        # 当只有根节点且根节点的值等于expectNumber，直接返回根节点列表
         if not root.left and not root.right and root.val == expectNumber:
             return [[root.val]]
-
+        # 否则
         else:
             expectNumber -= root.val
+            # 递归调用root的左节点
             left = self.FindPath(root.left, expectNumber)
+            # 递归调用root的右节点
             right = self.FindPath(root.right, expectNumber)
-
+            # 使用列表推导式保存节点值
             result = [[root.val] + i for i in left]
             for i in right:
                 result.append([root.val] + i)
-
+        # 排序result列表
         return sorted(result, key=lambda x: -len(x))
