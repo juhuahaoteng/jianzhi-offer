6ecbd22b3227a64cc252a4b2eb0ffc86dfef8bbe
lidianxiang <ldx2703@gmail.com>
2020-03-12 16:42:57 +0800

    包含min函数的栈

--- 包含min函数的栈.py
+++ 包含min函数的栈.py
@@ -12,25 +12,30 @@ class Solution:
         self.minStack = []
 
     def push(self, node):
-        # write code here
+        # 先将节点压入数据栈中
         self.stack.append(node)
+        # 判断最小栈是否为空或者节点值小于当前最小值
         if self.minStack == [] or node < self.min():
+            # 压入最小栈中
             self.minStack.append(node)
+        # 否则
         else:
             temp = self.min()
+            # 最小栈压入刚弹出的最小值
             self.minStack.append(temp)
 
     def pop(self):
-        # write code here
+        # 判断这个数据栈为空或者最小栈为空，直接返回
         if self.stack is None or self.minStack is None:
             return None
+        # 否则正常弹出数值
         self.minStack.pop()
         self.stack.pop()
 
     def top(self):
-        # write code here
+        # 返回数据占的最上面的值
         return self.stack[-1]
 
     def min(self):
-        # write code here
+        # 返回最小栈的最上面的值
         return self.minStack[-1]
